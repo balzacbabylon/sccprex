abca0845ab8e0db0ccf46a09393e1315de9b73b8
hossenalyazgi@protonmail.com <hossenalyazgi@protonmail.com>
2025-08-05 11:42:39 +0000

    finished adder

--- ch2/2-4-b.cpp
+++ ch2/2-4-b.cpp
@@ -1,5 +1,3 @@
-
-
 #include <iostream>
 
 int main(int argc, char* argv[]){
@@ -9,25 +7,29 @@ int main(int argc, char* argv[]){
     unsigned short mask = 1;
     unsigned short carry = 0;
     unsigned short op1, op2;
-    unsigned short sum1 = 0;
+    unsigned short sum = 0;
 
-    for(int i = 0; i < 8;i++){
+    for(int i = 0; i <= 8;i++){
         
-        op1 = b1 & ( mask << i);
-        op2 = b2 & ( mask << i);
-        std::cout << "---------------" << std::endl;
-        std::cout << "mask is: " << (mask << i) << std::endl;
-        std::cout << "op1 is: " << op1 << std::endl;
-        std::cout << "op2 is: " << op2 << std::endl;
+        op1 = b1 & mask;
+        op2 = b2 & mask;
+        
+        //std::cout << "---------------" << std::endl;
+        //std::cout << "mask is: " << mask << std::endl;
+        //std::cout << "op1 is: " << op1 << std::endl;
+        //std::cout << "op2 is: " << op2 << std::endl;
+        
 
-        sum1 |= (op1 ^ op2 ) | carry;
-        std::cout << "sum is: " << sum1 << std::endl;
-        carry = ( op1 & op2 ) << 1 ;  
-        std::cout << "carry is: " << carry << std::endl;
+        sum |= (op1 ^ op2 ) ^ carry;
+        //std::cout << "sum is: " << sum << std::endl;
+        carry = ( (op1 & op2) | (op1 & carry) | (op2 & carry)) << 1 ;  
+        //std::cout << "carry is: " << carry << std::endl;
+
+        mask <<= 1;
     }
     
-    std::cout << b1 << " + " << b2 << " is equal to: " << sum1 << std::endl;
-
+    std::cout << b1 << " + " << b2 << " is actually equal to: " << b1 + b2 << std::endl;
+    std::cout << b1 << " + " << b2 << " is equal to: " << sum << std::endl;
 
     return 0;
 }
